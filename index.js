88041b29af62816584186988cab9361eb1fedc60
IngSaray18 <pedrosaray18@hotmail.com>
2023-10-16 11:20:36 -0600

    NEW COMMIT

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
-const express = require('express');
-const cors = require('cors');
+const mongoose = require("mongoose");
+const express = require("express");
+const cors = require("cors");
+const passport = require("passport");
+const passportLocal = require("passport-local").Strategy;
+const cookieParser = require("cookie-parser");
+const bcrypt = require("bcryptjs");
+const session = require("express-session");
+const bodyParser = require("body-parser");
 const app = express();
+
+
+//conecxion a base de datos
+// URL de conexión a la base de datos MongoDB
+const url = 'mongodb://localhost:27017'; // Cambia esto según tu configuración
+
 
 // Habilitar CORS para todas las solicitudes
 app.use(cors());
@@ -13,7 +26,7 @@ const ejecutarRoute = require('./routes/ejecutar');
 app.use('/backup', backupRoute);
 app.use('/ejecutar', ejecutarRoute);
 
-const port = 4000;
+const port = 3000;
 
 app.listen(port, () => {
   console.log(`Servidor Express en ejecución en el puerto ${port}`);
--- routes/backup.js
+++ routes/backup.js
@@ -11,13 +11,13 @@ const dbConfig = {
     options: {
       encrypt: true,
       trustServerCertificate: true, // Para conexiones seguras
-      requestTimeout: 120000
+      requestTimeout: 1200000
     },
   };
 
 const BBDD = 'VentanillaWeb';
 
-router.get('/backup-database', async (req, res) => {
+router.get('/backup-databaseVentanillaWeb', async (req, res) => {
   try {
     // Conectar a la base de datos
     await sql.connect(dbConfig);
@@ -28,8 +28,8 @@ router.get('/backup-database', async (req, res) => {
 
     // T-SQL para realizar un respaldo de la base de datos
     const backupScript = `
-      BACKUP DATABASE ${BBDD}
-      TO DISK = 'G:/RespaldosProduccion/Respaldo${BBDD}${currentDate}.bak'
+      BACKUP DATABASE VentanillaWeb
+      TO DISK = 'G:/RespaldosProduccion/RespaldoVentanillaWeb${currentDate}.bak'
       WITH FORMAT, MEDIANAME = 'BD_SeguirdadBackup', NAME = 'BD_Seguridad-Full Database Backup';
     `;
 
@@ -40,11 +40,101 @@ router.get('/backup-database', async (req, res) => {
     await sql.close();
     console.log('Conexión cerrada');
 
-    res.json({ message: 'Respaldo de base de datos exitoso', filename: `${BBDD}${currentDate}.bak` });
+    res.json({ message: 'Respaldo de base de datos exitoso', filename: `VentanillaWeb${currentDate}.bak` });
   } catch (error) {
     console.error('Error al conectar o realizar el respaldo:', error);
     res.status(500).json({ error: 'Error al realizar el respaldo de la base de datos' });
   }
 });
 
+
+router.get('/backup-databaseBDSeg', async (req, res) => {
+  try {
+    // Conectar a la base de datos
+    await sql.connect(dbConfig);
+    console.log('Conexión exitosa');
+
+    // Obtener la fecha actual en formato "yyyymmdd"
+    const currentDate = moment().format('YYYYMMDD');
+
+    // T-SQL para realizar un respaldo de la base de datos
+    const backupScript = `
+      BACKUP DATABASE BD_Seguridad
+      TO DISK = 'G:/RespaldosProduccion/RespaldoBDSeguridad${currentDate}.bak'
+      WITH FORMAT, MEDIANAME = 'BD_SeguirdadBackup', NAME = 'BD_Seguridad-Full Database Backup';
+    `;
+
+    // Ejecutar el script de respaldo
+    const result = await sql.query(backupScript);
+
+    // Cerrar la conexión
+    await sql.close();
+    console.log('Conexión cerrada');
+
+    res.json({ message: 'Respaldo de base de datos exitoso', filename: `BD_Seguridad${currentDate}.bak` });
+  } catch (error) {
+    console.error('Error al conectar o realizar el respaldo:', error);
+    res.status(500).json({ error: 'Error al realizar el respaldo de la base de datos' });
+  }
+});
+
+router.get('/backup-databaseCajaAhorro', async (req, res) => {
+  try {
+    // Conectar a la base de datos
+    await sql.connect(dbConfig);
+    console.log('Conexión exitosa');
+
+    // Obtener la fecha actual en formato "yyyymmdd"
+    const currentDate = moment().format('YYYYMMDD');
+
+    // T-SQL para realizar un respaldo de la base de datos
+    const backupScript = `
+      BACKUP DATABASE CajaAhorro
+      TO DISK = 'G:/RespaldosProduccion/RespaldoCajaAhorro${currentDate}.bak'
+      WITH FORMAT, MEDIANAME = 'CajaAhorroBackup', NAME = 'CajaAhorro-Full Database Backup';
+    `;
+
+    // Ejecutar el script de respaldo
+    const result = await sql.query(backupScript);
+
+    // Cerrar la conexión
+    await sql.close();
+    console.log('Conexión cerrada');
+
+    res.json({ message: 'Respaldo de base de datos exitoso', filename: `CajaAhorro${currentDate}.bak` });
+  } catch (error) {
+    console.error('Error al conectar o realizar el respaldo:', error);
+    res.status(500).json({ error: 'Error al realizar el respaldo de la base de datos' });
+  }
+});
+
+router.get('/backup-databaseCajaAhorroT', async (req, res) => {
+  try {
+    // Conectar a la base de datos
+    await sql.connect(dbConfig);
+    console.log('Conexión exitosa');
+
+    // Obtener la fecha actual en formato "yyyymmdd"
+    const currentDate = moment().format('YYYYMMDD');
+
+    // T-SQL para realizar un respaldo de la base de datos
+    const backupScript = `
+      BACKUP DATABASE CajaAhorroT
+      TO DISK = 'G:/RespaldosProduccion/RespaldoCajaAhorroT${currentDate}.bak'
+      WITH FORMAT, MEDIANAME = 'CajaAhorroTBackup', NAME = 'CajaAhorroT-Full Database Backup';
+    `;
+
+    // Ejecutar el script de respaldo
+    const result = await sql.query(backupScript);
+
+    // Cerrar la conexión
+    await sql.close();
+    console.log('Conexión cerrada');
+
+    res.json({ message: 'Respaldo de base de datos exitoso', filename: `CajaAhorroT${currentDate}.bak` });
+  } catch (error) {
+    console.error('Error al conectar o realizar el respaldo:', error);
+    res.status(500).json({ error: 'Error al realizar el respaldo de la base de datos' });
+  }
+});
 module.exports = router;
